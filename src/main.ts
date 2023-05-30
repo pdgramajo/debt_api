f112f28fa3675ff4704e910a34ac797e9bf31432
Pablo Gramajo <pdgramajo@gmail.com>
2023-05-29 22:55:49 -0300

    morgan was implemented

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('combined'));
 
   app.useGlobalPipes(new ValidationPipe());
 
